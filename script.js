2cac0f51b2ea39676f1ed90097d2cb781650cd99
Jacob <51315646+Jac00000b@users.noreply.github.com>
2019-07-19 15:51:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+var style = document.createElement("style");
+style.type = "text/css";
+var head = document.head;
+head.appendChild(style);
+style.sheet.insertRule("* { color: #aaa !important; background-color: #333 !important; border-color: currentColor !important; }");
+completion(true);
